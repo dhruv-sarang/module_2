40305ad7af7ed9284565768f44ce4d0d27c56f1b
DHRUV SARANG <148605974+dhruv-sarang@users.noreply.github.com>
2023-10-23 16:31:36 +0530

    Update pr_20_19.dart

--- pr_20_19.dart
+++ pr_20_19.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 void main(){
   stdout.write('Enter No. : ');
   var n = int.parse(stdin.readLineSync().toString()), s=1;
-  // int s=1;
   for(int i=0; i<n; i++){
     for(int j=1; j<=i+1; j++) {
       stdout.write('${s++}\t');
