392a5a95e4e0736871dcbe99b2939d76aead85b9
DHRUV SARANG <148605974+dhruv-sarang@users.noreply.github.com>
2023-10-23 16:26:33 +0530

    Delete pr_1.dart

--- pr_1.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  print('Name\t\t:\tDhruv Sarang');
-  print('DOB\t\t\t:\t24/11/1995');
-  print('Age\t\t\t:\t27');
-  print('Adderess\t:\t1/751, Kharwawad, Nanpura, Surat-395001.');
-}
